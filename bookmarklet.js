fc533c5b7fee45ae3b7de6cf7ed4a958aa6ed9e1
Mitsuya.WATANABE <watanabe@gocco.co.jp>
2019-01-26 11:13:19 +0900

    Make code clean

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,64 +1,49 @@
-// javascript:(
-//   function(){
-//       var highlights = document.getElementsByClassName("kp-notebook-highlight");
-//       var highlights_array = [];
-//       for (var i = 0; i < highlights.length; i++) {
-//         highlights_array.push(highlights[i].innerText);
-//       }
-//       var results = Array.prototype.slice.call(highlights_array);
-//       c = window.open();
-//       c.document.open();
-//       for(j=0; j < results.length; j++) {
-//           c.document.write(">" + results[j]);
-//           c.document.write("<br>");
-//           c.document.write("<br>");
-//       }
-//       c.document.close()
-//     }
-//  )();
-
-
  javascript:
 Scrapbox="https://scrapbox.io/mitsuya/";
 url="https://read.amazon.co.jp/kp/notebook";
-if(location.href.indexOf(url)==0){
-  title=document.getElementById("annotation-scroller").querySelectorAll("h3.kp-notebook-metadata,h5.kp-notebook-metadata,#highlight,#note");
-  author=document.getElementById("annotation-scroller").querySelectorAll(".a-spacing-none,h5.kp-notebook-metadata,#highlight,#note");
-  s="";
-  sss = "";
-  for(i=0;i<title.length;i++) sss += title[i].innerText+"\n\n";
+if(location.href.indexOf(url) == 0) {
+  var titleElement=document.getElementById("annotation-scroller").querySelectorAll("h3.kp-notebook-metadata");
+  var title =titleElement[0].innerText;
 
-  var highlights = document.getElementsByClassName("kp-notebook-highlight");
-  var highlights_array = [];
+  var authorElement=document.getElementById("annotation-scroller").querySelectorAll(".a-spacing-none");
+  var authors = authorElement[0].innerText;  
+  var content ="";
 
-  var quotation = document.querySelectorAll("#annotationHighlightHeader");
-  var quotation_array = [];
+  var highlightNodes = document.getElementsByClassName("kp-notebook-highlight");
+  var highlights = [];
+
+  var quotationNodes = document.querySelectorAll("#annotationHighlightHeader");
+  var quotationPages = [];
   
-  for (var i = 0; i < highlights.length; i++) {
-    highlights_array.push(highlights[i].innerText);
+  for (var i = 0; i < highlightNodes.length; i++) {
+    highlights.push(highlightNodes[i].innerText);
   }
 
-  for (var i = 0; i < quotation.length; i++) {
-    quotation_array.push(quotation[i].innerText);
+  for (var i = 0; i < quotationNodes.length; i++) {
+    quotationPages.push(quotationNodes[i].innerText);
   }
   
-  b="["+document.querySelector(".kp-notebook-printable img").src+" "+document.querySelector(".kp-notebook-printable").href+"]";  
-  s += b;
-  s += "\n";
-  s += author[0].innerText;
-  
-  s += "\n\n";
-  var results = Array.prototype.slice.call(highlights_array);
-  var results2 = Array.prototype.slice.call(quotation_array);
-  for(j=0; j < results.length; j++) {
-      s += results2[j];
-      s += "\n";
-      s += ">" + results[j];
-      s += "\n";
+  var banner = "[" + document.querySelector(".kp-notebook-printable img").src + " " + document.querySelector(".kp-notebook-printable").href + "]";  
+  content += banner;
+  content += "\n";
+  content += "By: " + authors;  
+  content += "\n\n";
+
+  for(var i = 0; i < quotationPages.length; i++) {
+    content += quotationPages[i];
+    content += "\n";
+    content += ">" + highlights[i];
+    content += "\n";
   }
 
-  s += "#readings";
-  t =sss.split("\n")[0];
-  url=Scrapbox+encodeURIComponent(t)+"?body=" + encodeURIComponent(s);
+  var links = "#readings" + " ";  
+  authorList = authors.split('、');
+  for (var i = 0; i < authorList.length; i++) {
+    links += "#" + authorList[i] + " ";
+  }
+  content += links;
+
+  url = Scrapbox + encodeURIComponent(title) + "?body=" + encodeURIComponent(content);
 }
-location=url;
+
+location = url;
